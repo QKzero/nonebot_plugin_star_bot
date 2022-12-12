178fb99a403a1be08ff71e97b1861e9f93f37dd2
QKzero <123658730@qq.com>
2022-12-12 10:24:09 +0800

    日志等级调整

--- nonebot_plugin_star_bot/log.py
+++ nonebot_plugin_star_bot/log.py
@@ -1,18 +1,7 @@
-import os
-from nonebot.log import logger as _logger
+from nonebot.log import logger
 
 from . import config
 
-logger = _logger
-
-logger.add(config.resource_mkdir / 'logs' / '{time:YYYY-MM-DD}.log', level='WARNING', rotation='0:00')
-
-
-@logger.catch
-def index_error(custom_list: list):
-    for index in range(len(custom_list)):
-        index_value = custom_list[index]
-        if custom_list[index] < 2:
-            custom_list.remove(index_value)
-
-        print(index_value)
+logger.add(config.resource_mkdir / 'logs' / 'INFO' / '{time:YYYY-MM-DD}.log', level='INFO', rotation='0:00')
+logger.add(config.resource_mkdir / 'logs' / 'WARNING' / '{time:YYYY-MM-DD}.log', level='WARNING', rotation='0:00')
+logger.add(config.resource_mkdir / 'logs' / 'ERROR' / '{time:YYYY-MM-DD}.log', level='ERROR', rotation='0:00')
